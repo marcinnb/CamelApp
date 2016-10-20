b28c9779b57389f937ce73532e2b462b516d87d1
Marcin Bronecki <marcinn1993@gmail.com>
2016-10-20 08:41:22 +0200

    Add configuration class

---
+++ src/main/java/com/app/camel/Config.java
@@ -0,0 +1,9 @@
+package com.app.camel;
+
+public class Config {
+
+    public static final String URL = "jdbc:mysql://localhost:3306/library?serverTimezone=UTC";
+    public static final String USER = "root";
+    public static final String PASSWORD = "root";
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+}
