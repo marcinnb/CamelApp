70747357f74fb293cec5eade14acc110254458c5
Marcin Bronecki <marcinn1993@gmail.com>
2016-10-20 15:21:42 +0200

    Delete comments

--- src/main/java/com/app/camel/Application.java
+++ src/main/java/com/app/camel/Application.java
@@ -12,26 +12,11 @@ import java.sql.DriverManager;
 public class Application {
     public static void main(String[] args) throws Exception {
 
-       /* String user = Config.USER;
-        String password = Config.PASSWORD;
-        String url = Config.URL;
-        String driver = Config.DRIVER;
-
-        Class.forName(driver).newInstance();
-        DSLContext dslContext;
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            dslContext = DSL.using(connection, SQLDialect.MYSQL);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
-        CamelContext context= new DefaultCamelContext();
-        RestfulRoute restfulRoute= new RestfulRoute();
+        CamelContext context = new DefaultCamelContext();
+        RestfulRoute restfulRoute = new RestfulRoute();
         context.addRoutes(restfulRoute);
         context.start();
         Thread.sleep(50000);
         context.stop();
-
-
     }
 }
