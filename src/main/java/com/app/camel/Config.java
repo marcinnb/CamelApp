7bed17e90cd82582941201a3f322d9cc7c6c3976
Marcin Bronecki <marcinn1993@gmail.com>
2016-10-20 08:45:23 +0200

    Add database configuration class

--- src/main/java/com/app/camel/Config.java
+++ src/main/java/com/app/camel/Config.java
@@ -6,4 +6,5 @@ public class Config {
     public static final String USER = "root";
     public static final String PASSWORD = "root";
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
 }
