b03b4aadee790bf814b2c61a036a1690bc64b1d4
Marcin Bronecki <marcinn1993@gmail.com>
2016-10-21 12:18:33 +0200

    Register database driver

--- src/main/java/com/app/camel/Application.java
+++ src/main/java/com/app/camel/Application.java
@@ -6,6 +6,8 @@ import org.apache.camel.impl.DefaultCamelContext;
 public class Application {
     public static void main(String[] args) throws Exception {
 
+        Class.forName(Config.DRIVER).newInstance();
+
         CamelContext context = new DefaultCamelContext();
         RestfulRoute restfulRoute = new RestfulRoute();
         context.addRoutes(restfulRoute);
