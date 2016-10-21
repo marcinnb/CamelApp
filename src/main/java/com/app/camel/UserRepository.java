1719a56ff92b71bc68fe2455c12bfb3984e86598
Marcin Bronecki <marcinn1993@gmail.com>
2016-10-21 12:15:59 +0200

    Add UserRepository interface

---
+++ src/main/java/com/app/camel/UserRepository.java
@@ -0,0 +1,14 @@
+package com.app.camel;
+
+public interface UserRepository {
+
+    public String getAllUsers() throws ClassNotFoundException, IllegalAccessException, InstantiationException;
+
+    public String getUserById(Integer id) throws ClassNotFoundException, IllegalAccessException, InstantiationException;
+
+    public void addUser(String firstName, String lastName, String email, UserStatus isActive) throws ClassNotFoundException, IllegalAccessException, InstantiationException;
+
+    public void deleteUser(Integer id) throws ClassNotFoundException, IllegalAccessException, InstantiationException;
+
+    public void updateUserWithId(Integer id, String firstName, String lastName, String email, UserStatus isActive) throws ClassNotFoundException, IllegalAccessException, InstantiationException;
+}
